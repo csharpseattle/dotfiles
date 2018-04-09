d5c0e19d85f9d4356968ebb4c3a631ee21bb7cb6
Chris Sharp <git@ctsharp.org>
2018-04-09 15:21:44 -0700

    add brew cask installs

---
+++ macosx/brewSetup.sh
@@ -0,0 +1,22 @@
+brew install wget
+
+brew cask install google-chrome
+brew cask install emacs
+brew cask install private-internet-access
+brew cask install Banktivity
+brew cask install diffmerge
+brew cask install pdftotext
+brew cask install charles
+
+#
+# Install Bazel for tensorflow builds
+#
+brew cask install caskroom/versions/java8
+brew install bazel
+
+brew cask install slack
+brew cask install whatsapp
+
+brew install graphicsmagick
+brew install ffmpeg
+brew cask install sketchup
