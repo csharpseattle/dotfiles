0159fbeb264d08342c42a07315c76248561f2b65
Chris Sharp <git@ctsharp.org>
2018-10-15 09:48:11 -0700

    Monokai Theme in emacs

--- emacs/init.el
+++ emacs/init.el
@@ -42,7 +42,8 @@
 ;;
 ;;  Use color theme misterioso
 ;;
-(load-theme 'misterioso)
+(load-theme 'monokai t)
+;;;(load-theme 'misterioso)
 
 
 ;;
@@ -137,7 +138,17 @@
 ;; If you edit it by hand, you could mess it up, so be careful.
 ;; Your init file should contain only one such instance.
 ;; If there is more than one, they won't work right.
-(custom-set-variables '(package-selected-packages (quote (counsel))))
-(custom-set-faces)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (monokai-theme counsel))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (provide 'init)
